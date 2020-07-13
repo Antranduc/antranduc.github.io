095c65dbf4fcd9bff4139203ef89a46ea4a05ff6
Anthony Tranduc <anthonytranduc@gmail.com>
2020-07-12 23:46:20 -0400

    Updates

---
+++ component---src-pages-about-js-05d886b766d1ef7e0caf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),u=n("Bl7J"),c=n("vrFN");t.default=function(){return r.a.createElement(u.a,null,r.a.createElement(c.a,{title:"About"}))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-05d886b766d1ef7e0caf.js.map
